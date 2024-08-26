3d426ad03b7bbcee7c335c1d8722de91d8aea2b1
Christopher Boyle <christopher.boyle.101@strath.ac.uk>
2024-08-26 15:17:32 +0100

    whitespace

--- src/api.rs
+++ src/api.rs
@@ -3,7 +3,7 @@ use std::net::Ipv4Addr;
 use crate::ork::Ork;
 
 
-pub async fn run_api(ip: Ipv4Addr, port: u16,ork: Ork) {
+pub async fn run_api(ip: Ipv4Addr, port: u16, ork: Ork) {
     let app = axum::Router::new()
         .with_state(ork);
     let listener = tokio::net::TcpListener::bind((ip, port)).await.unwrap();
